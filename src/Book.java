b3fc46b2e22fa4aead3585beb3e151f3ec517d78
nikitamavrodiy <mna1998@mail.ru>
2022-10-06 23:32:15 +0300

    homework completed.

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
+
 public class Book {
-    private String name;
+    private final String name;
     private int yearPublishing;
-    private Author authorName;
+    private final Author authorName;
 
     public Book(String name, int yearPublishing, Author authorName) {
         this.name = name;
@@ -20,8 +22,25 @@ public class Book {
     public Author getAuthorName() {
         return this.authorName;
     }
+
     public void setYearPublishing(int yearPublishing) {
         this.yearPublishing = yearPublishing;
     }
 
+    public String toString() {
+        return this.name + " by " + this.authorName + " published in " + this.yearPublishing;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublishing == book.yearPublishing && Objects.equals(name, book.name) && Objects.equals(authorName, book.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, yearPublishing, authorName);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Author stepan = new Author("Stepan", "Kuznetsov");
         printAuthor(stepan);
+        Author stepan2 = new Author("Stepan", "Kuznetsov");
+        printAuthor(stepan2);
+        System.out.println("stepan.equals(stepan2) = " + stepan.equals(stepan2));
+
         Author roman = new Author("Roman", "Zvezdniy");
         printAuthor(roman);
 
@@ -17,10 +21,10 @@ public class Main {
         printBook(megaBook);
     }
     public static void printBook(Book nameBook) {
-        System.out.println(nameBook.getName() + " " + nameBook.getYearPublishing() + " " + nameBook.getAuthorName());
+        System.out.println("Книга - " + nameBook.getName() + " " + nameBook.getYearPublishing() + " " + nameBook.getAuthorName());
     }
 
     public static void printAuthor(Author nameAuthor) {
-        System.out.println(nameAuthor.getName()+" "+nameAuthor.getSurname());
+        System.out.println("Автор - "+nameAuthor.getName()+" "+nameAuthor.getSurname());
     }
 }
